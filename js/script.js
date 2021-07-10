5e043d39b735c2aea3c3b217c25a10a29e4fa16d
Huseyn <85623387+Huseyn-SomeGuy@users.noreply.github.com>
2021-07-10 19:19:22 +0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function myFunction() {
+   var btn = document.querySelector(".btn");
+   btn.classList.toggle("active");
+   var share = document.querySelector(".content__share");
+   share.classList.toggle("share__display");
+}
