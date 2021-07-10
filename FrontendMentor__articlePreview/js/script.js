e35e8d4e99800ef773d50aadc396cdb9a972efaf
Huseyn <85623387+Huseyn-SomeGuy@users.noreply.github.com>
2021-07-10 19:18:59 +0400

    Delete FrontendMentor__articlePreview directory

--- FrontendMentor__articlePreview/js/script.js
+++
@@ -1,6 +0,0 @@
-function myFunction() {
-   var btn = document.querySelector(".btn");
-   btn.classList.toggle("active");
-   var share = document.querySelector(".content__share");
-   share.classList.toggle("share__display");
-}
